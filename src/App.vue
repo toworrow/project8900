3fc93677f6ee2e6e09390ba29745f5b43e99b412
toworrow <1482965405@qq.com>
2019-07-10 19:36:30 +0800

    fixed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
 app
+<router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import ELEMENT from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router'
 Vue.use(ELEMENT)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
